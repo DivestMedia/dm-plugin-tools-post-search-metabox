605619fd8fc68906b8a24bb34747d74f14a33b79
Ralph John Galindo <ralphjesy@gmail.com>
2016-08-09 11:39:22 +0800

    Removing Preg Replace on Variable Definitions

--- post-search-metabox.php
+++ post-search-metabox.php
@@ -1,7 +1,7 @@
 <?php if(!defined('ABSPATH')) die('Fatal Error');
 
 define( 'DM_PLUGIN_TOOL_PSM_NAME', 'Post Search Metabox' );
-define( 'DM_PLUGIN_TOOL_PSM_SLUG', strtolower(preg_replace("/[^a-zA-Z0-9/_|+ -]/", '', DM_PLUGIN_TOOL_PSM_NAME)));
+define( 'DM_PLUGIN_TOOL_PSM_SLUG', 'post-search-metabox');
 define( 'DM_PLUGIN_TOOL_PSM_PLUGIN_DIR',  __FILE__ );
 define( 'DM_PLUGIN_TOOL_PSM_PLUGIN_URL',  __FILE__ );
 define( 'DM_PLUGIN_TOOL_PSM_DEBUG' , true );
