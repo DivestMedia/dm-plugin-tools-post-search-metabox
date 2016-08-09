0401de2b5560024c8cae87ef580f6262971b192b
Ralph John Galindo <ralphjesy@gmail.com>
2016-08-09 11:18:31 +0800

    Including Main class file

--- post-search-metabox.php
+++ post-search-metabox.php
@@ -2,6 +2,8 @@
 
 define( 'DM_PLUGIN_TOOL_PSM_NAME', 'Post Search Metabox' );
 define( 'DM_PLUGIN_TOOL_PSM_SLUG', strtolower(preg_replace("/[^a-zA-Z0-9/_|+ -]/", '', DM_PLUGIN_TOOL_PSM_NAME)));
-define( 'DM_PLUGIN_TOOL_PSM_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
-define( 'DM_PLUGIN_TOOL_PSM_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+define( 'DM_PLUGIN_TOOL_PSM_PLUGIN_DIR',  __FILE__ );
+define( 'DM_PLUGIN_TOOL_PSM_PLUGIN_URL',  __FILE__ );
 define( 'DM_PLUGIN_TOOL_PSM_DEBUG' , true );
+
+require_once DM_PLUGIN_TOOL_PSM_PLUGIN_DIR . 'lib/class-post-search-metabox.php';
